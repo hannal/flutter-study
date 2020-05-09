75b1664644c6e139164f07282cd665153d0c3dfa
hannal <kay@hannal.net>
2020-05-09 22:35:51 +0900

    Add i am rich project.

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'I am Rich',
+          textAlign: TextAlign.center,
+        ),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+    ),
+  ),
+);
+
