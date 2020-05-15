3f225a892648fc9a9c992b7517a808726afab2a1
hannal <kay@hannal.net>
2020-05-15 20:21:03 +0900

    Add column widget

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -13,14 +13,27 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-//            margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
-            margin: EdgeInsets.fromLTRB(20.0, 50.0, 30.0, 40.0),
-            padding: EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: Text('hello world'),
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.white,
+                child: Text('container 1'),
+              ),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.blue,
+                child: Text('container 2'),
+              ),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.black26,
+                child: Text('container 3'),
+              ),
+            ],
           ),
         ), // notch에 대응.
       ),
