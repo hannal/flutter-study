96b6eabde36e1e2c1519eeb4270ff28ba29a2f94
hannal <kay@hannal.net>
2020-05-18 13:44:50 +0900

    Add Padding into Card

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -38,53 +38,57 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              Container(
+              Card(
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(
                   vertical: 10.0,
                   horizontal: 25.0,
                 ),
-                padding: EdgeInsets.all(10.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.phone,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      '+82 1234 1234 1234',
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontFamily: 'Source Sans Pro',
-                        fontSize: 20.0,
+                child: Padding(
+                  padding: EdgeInsets.all(25.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal,
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '+82 1234 1234 1234',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              Container(
+              Card(
                 color: Colors.white,
-                padding: EdgeInsets.all(10.0),
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email,
-                      color: Colors.teal.shade900,
-                    ),
-                    SizedBox(width: 10.0),
-                    Text(
-                      'kay@hannal.net',
-                      style: TextStyle(
-                        fontSize: 20.0,
+                child: Padding(
+                  padding: EdgeInsets.all(25.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
                         color: Colors.teal.shade900,
-                        fontFamily: 'Source Sans Pro',
                       ),
-                    ),
-                  ],
+                      SizedBox(width: 10.0),
+                      Text(
+                        'kay@hannal.net',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
