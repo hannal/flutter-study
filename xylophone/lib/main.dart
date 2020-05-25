7e5ca6c63658d98639b528bcde647cf79a7b5793
hannal <kay@hannal.net>
2020-05-25 18:14:46 +0900

    Add xylophone keys.

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -4,6 +4,12 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  void playSound(String filename) {
+    final AudioCache player = AudioCache();
+    player.play(filename);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,14 +19,37 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                 final AudioCache player = AudioCache();
-                 player.play('note1.wav');
-              },
-              child: Text('click me'),
-            )
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                color: Colors.red,
+                onPressed: () { playSound('note1.wav'); },
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () { playSound('note2.wav'); },
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () { playSound('note3.wav'); },
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () { playSound('note4.wav'); },
+              ),
+              FlatButton(
+                color: Colors.teal,
+                onPressed: () { playSound('note5.wav'); },
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () { playSound('note6.wav'); },
+              ),
+              FlatButton(
+                color: Colors.purple,
+                onPressed: () { playSound('note7.wav'); },
+              ),
+            ],
           ),
         ),
       ),
