6c636db044431d3793e3ee739b4545eb4cfa6160
hannal <kay@hannal.net>
2020-06-07 18:04:18 +0900

    Add result content layout.

--- bmi_calculator/lib/constants.dart
+++ bmi_calculator/lib/constants.dart
@@ -20,3 +20,14 @@ const kLargeButtonTextStyle = TextStyle(
   fontSize: 25.0,
   fontWeight: FontWeight.bold,
 );
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
--- bmi_calculator/lib/result_page.dart
+++ bmi_calculator/lib/result_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'reusable_card.dart';
+import 'constants.dart';
+
 class ResultPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -7,7 +10,31 @@ class ResultPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('BMI Calculator'),
       ),
-      body: Text('hello'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              child: Text('Your result', style: kTitleTextStyle),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              colour: kActiveCardColour,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text('Normal', style: kResultTextStyle),
+                  Text('26.7'),
+                  Text('You have'),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
