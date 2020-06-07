71a0481a39f8e3e1b38103caf51dae0d8fa0a671
hannal <kay@hannal.net>
2020-06-07 22:08:23 +0900

    Refactor split geolocation code to class

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,4 +1,6 @@
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+
 import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
@@ -8,27 +10,20 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   void getLocation() async {
-    Future<Position> position = Geolocator()
-        .getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.low,
-        );
-    position.then((pos) {
-      print(pos);
-    });
+    Location location = Location();
+    await location.getCurrentLocation();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
     );
   }
 }
