23a5a791e10719a3d46a0b48504e7794c30bfbdd
hannal <kay@hannal.net>
2020-06-07 22:22:11 +0900

    Add http

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import 'package:geolocator/geolocator.dart';
 
@@ -14,11 +15,25 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     getLocation();
+    getData(35, 139);
   }
 
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
+  }
+
+  void getData(double lat, double lon) async {
+    const appid = 'b6907d289e10d714a6e88b30761fae22';
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?appid=$appid&lat=$lat&lon=$lon',
+    );
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
