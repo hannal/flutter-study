67e6b66dc7b3c0f64252d6ade59e553b35550d22
hannal <kay@hannal.net>
2020-06-08 15:21:27 +0900

    Add get variable from Navigator.pop

--- clima/lib/screens/location_screen.dart
+++ clima/lib/screens/location_screen.dart
@@ -78,10 +78,14 @@ class _LocationScreenState extends State<LocationScreen> {
                     ),
                   ),
                   FlatButton(
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) {
-                        return CityScreen();
-                      }));
+                    onPressed: () async {
+                      var typedName = await Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) {
+                          return CityScreen();
+                        }),
+                      );
+                      print(typedName);
                     },
                     child: Icon(
                       Icons.location_city,
