090c2a8c7ff463fd398eefbd375bbb9189f50eab
hannal <kay@hannal.net>
2020-06-08 20:14:44 +0900

    Fix delete bouncing

--- flashchat/lib/screens/welcome_screen.dart
+++ flashchat/lib/screens/welcome_screen.dart
@@ -22,13 +22,6 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
     animation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
 
     controller.forward();
-    animation.addStatusListener((status) {
-      if (status == AnimationStatus.completed) {
-        controller.reverse(from: 1.0);
-      } else if (status == AnimationStatus.dismissed) {
-        controller.forward();
-      }
-    });
 
     controller.addListener(() {
       setState(() {});
