1df37a338d06a46f8a142055ef6dd440a1ec25fe
hannal <kay@hannal.net>
2020-06-08 16:42:25 +0900

    Add dropdown button.

--- bitcoin_ticker/lib/price_screen.dart
+++ bitcoin_ticker/lib/price_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'coin_data.dart';
+
 class PriceScreen extends StatefulWidget {
   @override
   _PriceScreenState createState() => _PriceScreenState();
@@ -42,7 +44,14 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+              onChanged: (String value) {},
+              value: 'USD',
+              items: [
+                DropdownMenuItem(child: Text('USD'), value: 'USD'),
+                DropdownMenuItem(child: Text('EUR'), value: 'EUR'),
+              ],
+            ),
           ),
         ],
       ),
