fbfa7e552d1e63a0e35e429eba944ca644950628
hannal <kay@hannal.net>
2020-06-11 23:07:52 +0900

    Add firebase auth and create user.

--- flashchat/lib/screens/registration_screen.dart
+++ flashchat/lib/screens/registration_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flashchat/components/rounded_button.dart';
 import 'package:flashchat/constants.dart';
+import 'chat_screen.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static final String id = 'registration';
@@ -11,7 +13,7 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
-
+  final _auth = FirebaseAuth.instance;
   String email;
   String password;
 
@@ -61,9 +63,18 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               RoundedButton(
                 title: 'Register',
                 colour: Colors.blueAccent,
-                onPressed: () {
-                  print(email);
-                  print(password);
+                onPressed: () async {
+                  try {
+                    final newUser = await _auth.createUserWithEmailAndPassword(
+                      email: email,
+                      password: password,
+                    );
+                    if (newUser != null) {
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+                  } catch (e) {
+                    print(e);
+                  }
                 },
               ),
             ],
