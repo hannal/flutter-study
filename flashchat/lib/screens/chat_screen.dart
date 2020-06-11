f724ed49319ab3b247e4e9ac42661cd8669a7e19
hannal <kay@hannal.net>
2020-06-11 23:08:50 +0900

    Add getCurrentUser to chat screen.

--- flashchat/lib/screens/chat_screen.dart
+++ flashchat/lib/screens/chat_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:flashchat/constants.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -9,6 +11,27 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final _auth = FirebaseAuth.instance;
+  FirebaseUser loggedInUser;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+    print(loggedInUser.email);
+  }
+
+  void getCurrentUser() async {
+    try {
+      final user = await _auth.currentUser();
+      if (user != null) {
+        loggedInUser = user;
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
