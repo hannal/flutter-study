faf15b6c4f57a44a16ffbb5bb5c6f001c480738e
hannal <kay@hannal.net>
2020-06-14 00:38:43 +0900

    Refactor remove unused.

--- todoey/lib/widgets/tasks_list.dart
+++ todoey/lib/widgets/tasks_list.dart
@@ -4,9 +4,6 @@ import 'package:flutter/material.dart';
 import 'task_tile.dart';
 
 class TaskList extends StatelessWidget {
-  const TaskList({
-    Key key,
-  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
